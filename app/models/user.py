54491a9927b108e3cc49024fb0d16feee9dc2e91
Artem <dalyvinchi@gmail.com>
2024-12-03 00:18:54 +0300

    Add second_surname to user model

--- app/models/user.py
+++ app/models/user.py
@@ -12,4 +12,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     surname = db.Column(db.String(50), nullable=False)
+    second_surname = db.Column(db.String(50), nullable=False)
     # role = db.Column(db.Enum(UserRole), nullable=True)
