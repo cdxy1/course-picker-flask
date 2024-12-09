3c44d10a6bb905975bee0bc7e9c57170d17dcff8
Artem <dalyvinchi@gmail.com>
2024-12-09 03:04:35 +0300

    Minor fix of main app

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,14 @@ from flask import Flask
 
 from app.config import Config
 from app.extensions import db, migrate
-from app.routes.user import user
-from app.routes.post import post
 from app.routes.main import main
+from app.routes.post import post
+from app.routes.user import user
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     app.register_blueprint(user)
     app.register_blueprint(post)
     app.register_blueprint(main)
