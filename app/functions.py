c7eb7d3037156fa11e292fa2f4a1e0e8af8fe7a2
Artem <dalyvinchi@gmail.com>
2024-12-09 03:07:36 +0300

    Add function for save pics

---
+++ app/functions.py
@@ -0,0 +1,16 @@
+import os.path
+import secrets
+
+from flask import current_app
+
+
+def save_image(pic):
+    if pic:
+        random_hex = secrets.token_hex(8)
+        _, f_ext = os.path.splitext(pic.filename)
+        picture_file_name = random_hex + f_ext
+        picture_path = os.path.join(current_app.config["SERVER_PATH"], picture_file_name)
+        pic.save(picture_path)
+        return picture_file_name
+    else:
+        return None
