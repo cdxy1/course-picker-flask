aa7c040f8316017d873049d242e7c8a01003f1d8
Artem <dalyvinchi@gmail.com>
2024-12-09 03:06:16 +0300

    Extend User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,19 @@
 from enum import Enum
 
+from flask_login import UserMixin
+
 from ..extensions import db
 
 
 class UserRole(Enum):
-    STUDENT = "Student"
-    PROFESSOR = "Professor"
+    STUDENT = "Студент"
+    PROFESSOR = "Препод"
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    second_surname = db.Column(db.String(50), nullable=False)
-    # role = db.Column(db.Enum(UserRole), nullable=True)
+    name = db.Column(db.String(200), nullable=False)
+    login = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    avatar = db.Column(db.String(250), nullable=True)
+    role = db.Column(db.Enum(UserRole), nullable=False)
