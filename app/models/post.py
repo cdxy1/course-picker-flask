655341fd0dad7f3138857b98d716f9905a7cfc14
Artem <dalyvinchi@gmail.com>
2024-12-12 01:24:01 +0300

    Add relationship to Post model

--- app/models/post.py
+++ app/models/post.py
@@ -6,6 +6,9 @@ from app.extensions import db
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     subject = db.Column(db.String(), nullable=False)
-    professor = db.Column(db.String(), nullable=False)
-    name = db.Column(db.String(200), nullable=False)
-    data = db.Column(db.DateTime, default=datetime.utcnow)
+    professor = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+
+    user = db.relationship('User', backref='posts')
