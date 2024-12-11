9d9e5f93db66f8894b1e52d58772be06e1410b9b
Artem <dalyvinchi@gmail.com>
2024-12-12 01:25:10 +0300

    Add db methods to choose_theme route

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_required
 
 from app.extensions import db
@@ -15,12 +15,22 @@ post = Blueprint("post", __name__)
 def choose_theme():
     form = ChooseThemeForm()
     user = User.query.filter_by(id=current_user.get_id()).first()
+
     form.professor.choices = [professor.name for professor in User.query.filter_by(role=UserRole.PROFESSOR).all()]
     if UserRole.STUDENT == user.role:
         if form.validate_on_submit():
-            pass
+            post = Post(subject=form.subject.data, professor=form.professor.data)
+            try:
+                print(post)
+                db.session.add(post)
+                db.session.commit()
+                flash("Тема отправлена успешно отправлена", "success")
+                return redirect(url_for("user.profile"))
+            except:
+                db.session.rollback()
+                flash("Что-то пошло не так", "danger")
+                return redirect(url_for("user.profile"))
         else:
-            print(type(current_user.role))
             return render_template("post/choose_theme.html", form=form)
     else:
         return redirect(url_for("main.index"))
