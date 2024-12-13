e4c561ac0b5a97ba3d5a6d1f6a3184f6924ad4b2
Artem <dalyvinchi@gmail.com>
2024-12-13 03:22:10 +0300

    Add grade column to post model

--- app/models/post.py
+++ app/models/post.py
@@ -7,6 +7,7 @@ class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     subject = db.Column(db.String(), nullable=False)
     professor_name = db.Column(db.String(200), nullable=True)
+    grade = db.Column(db.String(), nullable=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     student_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
